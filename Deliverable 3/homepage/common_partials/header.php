9c0e471fe52ba8d6f3550d9c1cf91f8f1e0f7f40
Kishan <kmrug025@uottawa.ca>
2022-03-04 21:55:32 -0500

    upload common partials for the website

---
+++ Deliverable 3/homepage/common_partials/header.php
@@ -0,0 +1,13 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+<link rel="stylesheet" href="styles.css">
+<link rel="icon" href="../images/logo2.jpg">
+
+<title>Rapid Grocers</title>
---
+++ Deliverable 3/homepage/common_partials/nav.php
@@ -0,0 +1,119 @@
+<nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-pink">
+    <div class="container-fluid">
+
+        <div class="col-2">
+            <div>
+                <a class="navbar-brand company" href="#">
+                    <img src="../images/logo2.jpg" alt="logo" width="40" height="30" class="mb-2">
+                    Rapid Grocers
+                </a>
+            </div>
+        </div>
+
+        <div class="col-6">
+            <div>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="text" placeholder="Search for any product here">
+                    <button class="btn searchButton" type="button">Search</button>
+                </form>
+            </div>
+
+        </div>
+        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+        <div class="col-2">
+
+            <div class="dropdown w-75">
+                <button class="btn btn-md dropdown-toggle box col-12" data-bs-toggle="dropdown">
+                    Category
+                </button>
+                <ul class="dropdown-menu category animate__animated animate__fadeIn">
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <img class="imageFit" src="../images/electronics.jpg" alt="electronics"><br>
+                            <span class="categoryText">
+                                Electronics
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <img src="../images/dairy.jpg" alt="dairy" class="imageFit"><br>
+                            <span class="categoryText">
+                                Dairy
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <img src="../images/fruits.jpg" alt="fruits" class="imageFit"><br>
+                            <span class="categoryText">
+                                Fruits
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <img src="../images/vegetables.jpg" alt="vegetables" class="imageFit"><br>
+                            <span class="categoryText">
+                                Vegetables
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <img src="../images/snacks.jpg" alt="snacks" class="imageFit"><br>
+                            <span class="categoryText">
+                                Snacks
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <img src="../images/beverages.jpg" alt="beverages" class="imageFit"><br>
+                            <span class="categoryText">
+                                Beverages
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-2">
+            <div class="dropdown w-75">
+                <button class="btn btn-md dropdown-toggle box col-9" data-bs-toggle="dropdown">Menu</button>
+                <ul class="dropdown-menu menu animate__animated animate__fadeIn">
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <span class="categoryText">
+                                Checkout
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                        </hr>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <span class="categoryText">
+                                Log Out
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                        </hr>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">
+                            <span class="categoryText">
+                                Contact Us
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
