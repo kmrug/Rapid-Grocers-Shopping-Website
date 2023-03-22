912dfb7bca2b98b33e8977602a8040f028d42345
Kishan <kmrug025@uottawa.ca>
2023-03-21 23:28:58 -0400

    remove google api key

--- Deliverable 4/homepage/google/googleAPI.php
+++ Deliverable 4/homepage/google/googleAPI.php
@@ -7,7 +7,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
-<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=AIzaSyDAdBqLlhoX7scG94JPyeUFCesaYgW1iQ0"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key="></script>
 <script>
 	var autocomplete;
 
